53c987d41c2cba73572fc3b3155cd8971cd17b4d
Rui Nabeshima <ruinabeshima0321@gmail.com>
2025-10-23 16:09:49 +0900

    Added game container and title component, with basic positional styling

---
+++ src/components/gamecontainer.jsx
@@ -0,0 +1,12 @@
+import Title from "./title";
+import "../styles/game.css"
+
+function GameContainer(){
+  return (
+    <div id="game-container">
+      <Title />
+    </div>
+  ); 
+}
+
+export default GameContainer
---
+++ src/components/title.jsx
@@ -0,0 +1,9 @@
+import "../styles/game.css"
+
+function Title(){
+  return (
+    <p id="title">F1 Drivers Memory Game</p>
+  ); 
+}
+
+export default Title
